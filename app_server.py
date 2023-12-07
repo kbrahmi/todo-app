71a26162fa79d349361898c28feb924c2bf86e15
LenoirEmmanuel <manu.lenoir66@gmail.com>
2023-12-07 11:54:18 +0100

    database.py et app_server.py

--- app_server.py
+++ app_server.py
@@ -2,9 +2,12 @@ from fastapi import FastAPI, responses
 
 from routers import user_router
 from routers import task_router
+from database import init_db
 
 
 app = FastAPI()
+
+init_db()
 
 app.include_router(user_router.router, prefix="/users", tags=["users"])
 app.include_router(task_router.router, prefix="/tasks", tags=["tasks"])
--- database.py
+++ database.py
@@ -1,3 +1,30 @@
 import sqlite3
 
-connection = sqlite3.connect('todo-app')
+
+def connect():
+    return sqlite3.connect('todo-app')
+
+
+def init_db():
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS user (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name VARCHAR NOT NULL,
+            password VARCHAR NOT NULL,
+            email VARCHAR NOT NULL UNIQUE
+        )
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS task (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title VARCHAR NOT NULL,
+            description VARCHAR,
+            status VARCHAR NOT NULL CHECK (status IN ('Not started', 'In progress', 'Completed')),
+            assignee_id INTEGER,
+            FOREIGN KEY (assignee_id) REFERENCES user(id)
+        )
+    ''')
+    conn.commit()
+    conn.close()
