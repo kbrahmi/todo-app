5b718f30d879bc846d3bea312e52c5a9814899c2
LenoirEmmanuel <manu.lenoir66@gmail.com>
2023-12-15 12:03:08 +0100

    fixe app_server and router ok

--- app_server.py
+++ app_server.py
@@ -11,9 +11,9 @@ app = FastAPI()
 
 init_db()
 
+app.include_router(login_router.router, prefix="/login", tags=["login"])
 app.include_router(user_router.router, prefix="/users", tags=["users"])
 app.include_router(task_router.router, prefix="/tasks", tags=["tasks"])
-app.include_router(login_router.router, prefix="/login", tags=["login"])
 
 
 @app.get("/", include_in_schema=False)
